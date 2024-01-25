21454497a562f88d8faf295ef4c3353c88c37764
Agyanim <71318355+Agyanim@users.noreply.github.com>
2024-01-25 23:45:11 +0000

    model added

--- counsellorApp/models.py
+++ counsellorApp/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class ClassTable(models.Model):
     class_name=models.CharField(max_length=255)
     
+    def __str__(self) -> str:
+        return self.class_name
+    
 class StudentTable(models.Model):
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     date_of_birth=models.DateField()
     class_name=models.ForeignKey(ClassTable,on_delete=models.CASCADE)
     guardian_Contact=models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.first_name +' '+ self.last_name
     
