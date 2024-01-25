731672aa9e645918a8e4e5752cb8892ab980cfa0
Agyanim <71318355+Agyanim@users.noreply.github.com>
2024-01-25 23:30:11 +0000

    model added

--- counsellorApp/admin.py
+++ counsellorApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ClassTable,StudentTable
 
 # Register your models here.
+admin.site.register(ClassTable)
+admin.site.register(StudentTable)
--- counsellorApp/models.py
+++ counsellorApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ClassTable(models.Model):
+    class_name=models.CharField(max_length=255)
+    
+class StudentTable(models.Model):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    date_of_birth=models.DateField()
+    class_name=models.ForeignKey(ClassTable,on_delete=models.CASCADE)
+    guardian_Contact=models.CharField(max_length=255)
+    
