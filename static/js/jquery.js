421755f980e4976c00c32f6784d85a0cfe382a18
agyanim <agyanimconsultancy@gmail.com>
2024-02-01 17:31:01 +0000

    animation

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,17 +1,36 @@
-
 // $('h1').css('background-color','green')
 
 //     $('#btn1').on('click',()=>{
 //         $('h1').css({'background-color':'red',
 //             'font-size':'10rem'
+
+//     })
+//     })
+
+const newImage = $("#pic1");
+const sendBtn = $("#btn1");
+
+let count=0
+sendBtn.on("click", () => {
+//   newImage.toggle()
+setInterval(() => {
+    newImage.animate({
+        left:'+=50',
+        height:'toggle'
+      },500)
+    count +=10
+    console.log(count)
+    if (count>50){
+        newImage.stop()
+    }
+}, 200);
+
+
+});
+newImage.on("click", () => {
+    newImage.stop()
+//   newImage.animate({
+//     left:'+=20'
+//   },500)
     
-//     })
-//     })
-
-
-const newImage=$('#pic1')
-const sendBtn=$('#btn1')
-
-sendBtn.on('click',()=>{
-    newImage.fadeOut(2000).fadeIn(2000)
-})
+});
